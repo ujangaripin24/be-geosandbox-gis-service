c28a111a73b0476c30d8b57788f69204520f8ee2
ujangaripin24 <ujangarifin06@gmail.com>
2026-09-17 14:34:27 +0700

    success add table user

---
+++ src/migrations/20260917072854-create-detail-users.js
@@ -0,0 +1,36 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("tbl_users", {
+      uuid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      username: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("tbl_users");
+  },
+};
